7cfc2fb1a87f40cded6f3c890f93e4529dbd51c9
oluxchai89 <oluchiomegor@gmail.com>
2022-10-18 14:32:48 -0700

    C-Functions-nested-loops-task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  print_alphabet-prints the alhpa in lowercase
+ */
+void print_alphabet(void)
+{
+	char la
+
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+
+	_putchar('\n');
+}
