c9d63ae952418a6024078b1895bb05752a9f5ff1
oluxchai89 <oluchiomegor@gmail.com>
2022-10-18 18:47:47 -0700

    C-Functions-nested-loops-task-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,15 @@
 
 /**
  * print_alphabet_x10- print 10times the alphabet, in lowercase
+ * Return: 0 success
  */
 
 void print_alphabet_x10(void)
 {
 	char letter, k;
 
-	for (k = 0;  k < 10; k++)
+	for (k = 0;  k <= 9; k++)
 	{
-
 		for (letter = 'a'; letter = 'z'; letter++)
 		{
 			_putchar(letter);
