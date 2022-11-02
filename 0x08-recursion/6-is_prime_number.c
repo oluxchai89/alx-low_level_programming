e94e87f5ef5347ad2a9aa8cdc312f5370af3e5e9
oluxchai89 <oluchiomegor@gmail.com>
2022-11-02 14:13:59 -0700

    0x08-c-recursion-task-6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,23 @@
+/*
+ * File: 0 is _prime_number.c
+ * Auth: sam tech
+ */
+
 #include "main.h"
 
-/**
- * sqrt2-Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
- *
- * Return: On success1 1.
- * On error, -1 is returned and errno is set appropriately.
- */
-int sqrt2(int a, int b)
-{
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
-}
-/**
- * _sqrt_recursion- returns the natural square root of n
- * @n: Number Integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _sqrt_recursion(int n)
-{
-	return (sqrt2(n, 1));
-}
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
+/**
+ * is_divisible-Checks if a nujmber is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the nujmber is divisible - 0.
+ * If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+}
